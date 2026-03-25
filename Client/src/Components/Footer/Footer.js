3c16fe655dbdf4fbeb07fb9a13f9b4eec4e01cfe
Amit Rawat <amitrawat@Amits-MacBook-Air.local>
2026-03-25 21:38:07 +0530

    Initial commit

---
+++ Client/src/Components/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div>
+        <Link className="logo-container" to="/">
+          <img className="navbar-logo" src={logo} alt="PawFinds Logo" />
+          <p>{props.title}</p>
+        </Link>
+      </div>
+      <div className="below-footer">
+        <p>
+          You can reach me at{" "}
+          <a className="mail-links" href="#">
+            amitrawat5@gmail.com
+          </a>
+        </p>
+        <p>
+          <a
+            className="contact-links"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="#"></i> Linkedin
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="#"></i> GitHub
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="#"></i> Instagram
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="#"></i> WhatsApp
+          </a>
+        </p>
+        <p>&copy; 2025 Amit Rawat</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
